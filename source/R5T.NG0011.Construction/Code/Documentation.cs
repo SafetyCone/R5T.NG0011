0505984d20070b88cd50694f93bbb84a880a55fa
davidcoats <david@getsafetycone.com>
2022-10-24 01:02:11 -0700

    Initial commit.

---
+++ source/R5T.NG0011.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0011.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.NG0011 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.NG0011/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0011
+{
+	/// <summary>
+	/// Microsoft.CSharp NuGet package selector (for the dynamic keyword).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
